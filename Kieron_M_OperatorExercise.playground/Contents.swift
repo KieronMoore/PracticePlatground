baa65af2f0d4836b2439153f111257d5c63b5ce8
KieronMoore <67976013+KieronMoore@users.noreply.github.com>
2020-07-14 13:30:31 -0500

    FifthCommit
    FifthCommit

---
+++ Kieron_M_OperatorExercise.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+let x = 5
+
+let y = 2
+
+var answer = x + y
+
+print(answer)
+
+answer = x - y
+
+print(answer)
+
+answer = x * y
+
+print(answer)
+
+answer = x / y
+
+print(answer)
